dd8e7558daf7c76c0d6d71d73e826e463e9f434c
jponce19 <contacto.jponce@gmail.com>
2019-09-09 17:22:01 -0300

    geolocalizacion complete desktop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'trackerDesk';
+  lat = 51.678418;
+  lng = 7.809007; 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'; 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBeTe5_ummkgMC28pZGOJwk9PEYdhq8hng'
+    })    
   ],
   providers: [],
   bootstrap: [AppComponent]
